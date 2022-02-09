9f55cf4c949760cf2905856cd1f1104922086ea9
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-02-09 16:07:53 -0500

    updating psuedo code

--- src/scripts/Scenes/TutorialScene.js
+++ src/scripts/Scenes/TutorialScene.js
@@ -4,13 +4,18 @@ export default class GameScene extends Phaser.Scene {
         super ({
             key: 'Tutorial'
         });
- 
+    //variable for amount of enemies// 
+    //variable for tutorial text, size and font = css//
     }
     Preload(){
-
+        //tutorial map image//
+        //enemy files enemy//
     }
     
     Create(){
-        
+        //text for controls//
+        //text for objectvie//
+        //text size and font equal to = css//
+        //enemy amount equal to a 'variable'
     }
 }
