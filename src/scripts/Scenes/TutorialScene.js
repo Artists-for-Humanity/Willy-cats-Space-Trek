4ec0c445f044a45957a88bc120d5b6a1868b393a
gordanliu <gordonliu2004@gmail.com>
2022-02-09 16:31:20 -0500

    tutorial scene pseudocode

--- src/scripts/Scenes/TutorialScene.js
+++ src/scripts/Scenes/TutorialScene.js
@@ -6,6 +6,8 @@ export default class GameScene extends Phaser.Scene {
         });
     //variable for amount of enemies// 
     //variable for tutorial text, size and font = css//
+    //variable for toggleMove 
+    //variable for toggleShoot 
     }
     Preload(){
         //tutorial map image//
@@ -13,9 +15,14 @@ export default class GameScene extends Phaser.Scene {
     }
     
     Create(){
-        //text for controls//
-        //text for objectvie//
-        //text size and font equal to = css//
-        //enemy amount equal to a 'variable'
+        //load into bunker
+        //text for controls, toggleMove on
+        //text to enter door//
+        //load into tutorial map
+        //load enemies
+        //text for shooting, toggleShoot on
     }
+
+    //create movementLock function using toggleMove
+    //create weaponLock function using toggleShoot
 }
