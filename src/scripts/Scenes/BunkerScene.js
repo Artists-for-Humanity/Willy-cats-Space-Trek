33ad3f23ec230a975716325818aed6e8b222f090
gordanliu <gordonliu2004@gmail.com>
2022-03-17 17:59:40 -0400

    removed BorderToggle
    removed variable from GameRule,
    border true/false used as a parameter (check previous commit)

--- src/scripts/Scenes/BunkerScene.js
+++ src/scripts/Scenes/BunkerScene.js
@@ -27,7 +27,7 @@ export default class BunkerScene extends Phaser.Scene {
 
   create() {
     this.background = this.add.image((this.game.config.width / 2) , (this.game.config.height /2), 'background' );
-    this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2);
+    this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2, true, false);
    
     this.anims.create({
       key: 'idle',
