089b81368c86fab1049bdc8141784272c35673e5
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-04-27 17:45:16 -0400

    minor shop update (#16)
    added shop templates and power up assets.

--- src/scripts/Scenes/ShopScene.js
+++ src/scripts/Scenes/ShopScene.js
@@ -13,28 +13,27 @@ export default class ShopScene extends Phaser.Scene {
 
   preload() {
     this.load.image('background', new URL('../../assets/RealBunker.png', import.meta.url).href);
-    this.load.image('BunkerBtn', new URL('../../assets/start-button.png', import.meta.url).href);
+    this.load.image('XBtn', new URL('../../assets/X.png', import.meta.url).href);
+    this.load.image('InvBG', new URL('../../assets/Menu_Screen.png', import.meta.url).href);
+    this.load.image('ItemDesc', new URL('../../assets/itemDesc.png', import.meta.url).href);
+
+
 
   }
 
   create() {
-    this.globalState.addUIBorder(this.scene.getIndex(this.key));
-    this.background = this.add.image(this.game.config.width / 2, this.game.config.height / 2 + 25, 'background');
-    this.bunkerBtn = this.add.image(900,300, 'BunkerBtn').setScale(0.5)
-    this.bunkerBtn.setInteractive();
-    this.bunkerBtn.on('pointerdown', () => {
+
+    this.background = this.add.image((this.game.config.width / 2), (this.game.config.height / 2), 'background');
+    this.XBtn = this.add.image(900,200, 'XBtn')
+    this.XBtn.setInteractive();
+    this.XBtn.on('pointerdown', () => {
+
       this.scene.start('BunkerScene');
     });
+    this.InvBG =  this.add.image(635, 360, 'InvBG' ).setScale(.85)
+    this.ItemDesc = this.add.image(250, 360, 'ItemDesc').setScale(.80)
 
-    this.ratHBtext = this.add.text(640, 450, 'interact', {
-      fontFamily: 'Space Mono',
-      fontSize: '24px',
-      fontStyle: 'bold',
-      fill: colors.white,
-      align: 'center',
-    })
-    this.ratHBtext.visible = false;
-
+  
     
   }
 
